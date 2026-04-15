3d4f901c60378f05eb6f6c3c3896604c1fb0774f
Ysaeka <binou75@gmail.com>
2026-04-15 10:26:15 +0200

    feat: ajout du healthcheck

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, healthcheck
 
 urlpatterns = [
     path("", home, name="home"),
+    path("health/", healthcheck, name="healthcheck"),
     path("admin/", admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def home(request):
     return HttpResponse("M Motors - application en ligne")
+
+
+def healthcheck(request):
+    return JsonResponse({"status": "ok"})
