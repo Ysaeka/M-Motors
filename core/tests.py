33a1c5992a002abd16029d15e168d65d1c612262
Ysaeka <binou75@gmail.com>
2026-04-15 10:45:42 +0200

    test: ajout du lint et du premier test Django

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CoreViewsTests(TestCase):
+    def test_healthcheck_returns_ok(self):
+        response = self.client.get(reverse("healthcheck"))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"status": "ok"})
