4d5ca8259eacdb40f98407a3b8c24395e75c5660
Ysaeka <binou75@gmail.com>
2026-04-24 04:32:46 +0200

    feat: ajout de la page detail vehicule

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Vehicle
 
@@ -7,3 +7,7 @@ from .models import Vehicle
 def vehicle_list(request):
     vehicles = Vehicle.objects.all().order_by("-created_at")
     return render(request, "catalog/vehicle_list.html", {"vehicles": vehicles})
+
+def vehicle_detail(request, pk):
+    vehicle = get_object_or_404(Vehicle, pk=pk)
+    return render(request, "catalog/vehicle_detail.html", {"vehicle": vehicle})
