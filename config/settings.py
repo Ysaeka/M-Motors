52620cc41507c7edccb0154391cbbf9895758f81
Ysaeka <binou75@gmail.com>
2026-04-30 08:12:45 +0200

    feat: add Django home page template

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "asset",
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
